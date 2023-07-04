3474f7a0184aee9e88c3e157b4f5328767457433
Ian Vega <lan@ciencias.unam.mx>
2023-07-04 16:12:32 -0600

    deploy3

--- deploy.sh
+++ deploy.sh
@@ -21,8 +21,12 @@ git add -A
 # Commit the changes
 git commit -m 'deploy'
 
+# Create the gh-pages branch
+git checkout -b gh-pages
+
+# Push the branch to the remote repository
 # Replace "<YOUR_USERNAME>" with your GitHub username
 # Replace "<YOUR_REPOSITORY>" with your repository name
-git push -f git@github.com:Lann892/AI-Model.git main:gh-pages
+git push -f git@github.com:Lann892/AI-Model.git gh-pages
 
 cd -
